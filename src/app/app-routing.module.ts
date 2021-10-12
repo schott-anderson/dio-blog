c52998110951ac5f0e0d97665b1d826ce337f602
schott-anderson <schott.anderson@hotmail.com>
2021-10-12 09:21:28 -0300

    Configura navbar, corrige link no script em angular.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
-    path: '/', redirectTo: 'home', pathMatch: 'full'
+    path: '', redirectTo: 'home', pathMatch: 'full'
   },
   {
     path: 'home', component: HomeComponent
